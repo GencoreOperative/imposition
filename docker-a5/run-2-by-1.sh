221ff0f27945f0ee94781872e0147fb58221fd20
Robert Wapshott <robert@wapshott.org.uk>
2020-02-17 10:34:08 +0000

    Fixing the print layout for A5 to match actual printer doublesided output.

--- docker-a5/run-2-by-1.sh
+++ docker-a5/run-2-by-1.sh
@@ -4,6 +4,6 @@
 
 # For 16 pages on four double sided sheets:
 find $WORK -iname "*-x*.pdf" | xargs java -cp /root/multivalent.jar tool.pdf.Impose \
-  -dim 1x2 -paper A4 -layout "16r,1r,15l,2l,14r,3r,13l,4l,12r,5r,11l,6l,10r,7r,9l,8l"
+  -dim 1x2 -paper A4 -layout "16r,1r,2r,15r,14r,3r,4r,13r,5r,12r,11r,6r,7r,10r,9r,8r"
 
 cp $WORK/*-up.pdf /mount
