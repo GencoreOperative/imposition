afedf957ce369c1b84d921ee9a78726448196679
Robert Wapshott <robert@wapshott.org.uk>
2020-02-19 00:03:07 +0000

    Fixed a problem with the A5 imposition.

--- docker-a5/run-2-by-1.sh
+++ docker-a5/run-2-by-1.sh
@@ -4,6 +4,6 @@
 
 # For 16 pages on four double sided sheets:
 find $WORK -iname "*-x*.pdf" | xargs java -cp /root/multivalent.jar tool.pdf.Impose \
-  -dim 1x2 -paper A4 -layout "16r,1r,2r,15r,14r,3r,4r,13r,5r,12r,11r,6r,7r,10r,9r,8r"
+  -dim 1x2 -paper A4 -layout "16r,1r,2r,15r,14r,3r,4r,13r,12r,5r,6r,11r,10r,7r,8r,9r"
 
 cp $WORK/*-up.pdf /mount
