1d1f7639911ad32d25fa4a8f530a5844eab13963
Robert Wapshott <robert@wapshott.org.uk>
2020-03-24 00:24:41 +0000

    Updating the A5 imposition to support variable signature size.

--- docker-a5/run-2-by-1.sh
+++ docker-a5/run-2-by-1.sh
@@ -1,9 +1,18 @@
-#!/bin/bash
+#!/bin/bash -xv
 
-. /root/1-signatures.sh
+. /root/0-copy.sh
+
+SIGNATURES=$1
+if [ -z "$SIGNATURES" ]; then
+	SIGNATURES=16
+fi
+echo "Signatures Size: $SIGNATURES"
+
+# Generate Signatures String
+SIG_STRING=$(java -jar /root/sequence-generator-0.1.jar $SIGNATURES)
 
 # For 16 pages on four double sided sheets:
-find $WORK -iname "*-x*.pdf" | xargs java -cp /root/multivalent.jar tool.pdf.Impose \
-  -dim 1x2 -paper A4 -layout "16r,1r,15l,2l,14r,3r,13l,4l,12r,5r,11l,6l,10r,7r,9l,8l"
+find $WORK -iname "*.pdf" | xargs java -cp /root/multivalent.jar tool.pdf.Impose \
+  -dim 1x2 -paper A4 -layout "$SIG_STRING"
 
 cp $WORK/*-up.pdf /mount
