14f069528bed48eae58d08b6b73a23a02e4eb3f4
Robert Wapshott <robert@wapshott.org.uk>
2026-04-18 10:10:58 +0100

    Improving the project and starting on support for A6 imposition

--- docker-a5/run-2-by-1.sh
+++ docker-a5/run-2-by-1.sh
@@ -2,17 +2,20 @@
 
 . /root/0-copy.sh
 
-SIGNATURES=$1
-if [ -z "$SIGNATURES" ]; then
-	SIGNATURES=16
-fi
-echo "Signatures Size: $SIGNATURES"
+COUNT=$(java -cp /root/multivalent.jar tool.pdf.Info $TARGET | grep "Page count:" | cut -d ' ' -f 3)
+echo "Page Count: $COUNT"
+
+#SIGNATURES=$1
+#if [ -z "$SIGNATURES" ]; then
+#	SIGNATURES=16
+#fi
+#echo "Signatures Size: $SIGNATURES"
 
 # Generate Signatures String
-SIG_STRING=$(java -jar /root/sequence-generator-0.1.jar $SIGNATURES)
+SIG_STRING=$(java -jar /root/sequence-generator-0.1.jar $COUNT)
 
 # For 16 pages on four double sided sheets:
 find $WORK -iname "*.pdf" | xargs java -cp /root/multivalent.jar tool.pdf.Impose \
-  -dim 1x2 -paper A4 -layout "$SIG_STRING"
+-dim 1x2 -paper A4 -layout "$SIG_STRING"
 
 cp $WORK/*-up.pdf /mount
--- sequence-generator/src/test/java/uk/co/gencoreoperative/imposition/A5ImpositionTest.java
+++ sequence-generator/src/test/java/uk/co/gencoreoperative/imposition/A5ImpositionTest.java
@@ -7,8 +7,14 @@ public class A5ImpositionTest {
     private final A5Imposition main = new A5Imposition();
 
     @Test
-    public void shouldMatchExpectedOutput() {
+    public void shouldMatchExpectedOutputFor16() {
         String result = main.generateSequence(16);
         Assertions.assertThat(result).isEqualTo("16r,1r,15l,2l,14r,3r,13l,4l,12r,5r,11l,6l,10r,7r,9l,8l");
+    }
+
+    @Test
+    public void shouldMatchExpectedOutputFor8() {
+        String result = main.generateSequence(8);
+        Assertions.assertThat(result).isEqualTo("8r,1r,7l,2l,6r,3r,5l,4l");
     }
 }
